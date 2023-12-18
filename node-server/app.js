be104a20e6238947c7881ec0fdf4af6b269b8a87
amit-sahani <amitg.one19@gmail.com>
2023-12-18 23:51:55 +0530

    sending basic response (html reponse)

--- node-server/app.js
+++ node-server/app.js
@@ -1,9 +1,21 @@
 const http = require('http');
 
 // this creates a server object
-const server = http.createServer((req, response)=>{
+const server = http.createServer((req, res)=>{
     //this callback will run every time an http request comes
     console.log('New request comes.')
+
+    console.log(`url=>${req.url}`, req.method, req.headers)
+
+    res.setHeader('Content-Type', 'text/html')
+    res.write('<html>')
+    res.write('<body>')
+    res.write('<h1>')
+    res.write('Hello from Node.js server response!!')
+    res.write('</h1>')
+    res.write('</body>')
+    res.write('</html>')
+    res.end()
 })
 
 // this listen method is used to bind and listen for incoming connections on a specified port and hostname
