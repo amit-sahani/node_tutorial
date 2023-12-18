b459217449e8fb65ab326837fba414d1e6ccef2e
amit-sahani <amitg.one19@gmail.com>
2023-12-18 19:23:06 +0530

    second commit: test module import and fs module's writeFileSync method

--- first_class/index.js
+++ first_class/index.js
@@ -1,1 +1,4 @@
-console.log('My first code in node')
+const fs  = require('fs')
+
+// create and write test to it. arg1 = file name, arg2 = test to write in file
+fs.writeFileSync('hello.txt', 'I am created from fs module in current path.')
