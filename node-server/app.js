2460ce945cfe22b2add623c4176af656e6c29a5b
amit-sahani <amitg.one19@gmail.com>
2023-12-19 00:20:43 +0530

    redirecting routes

--- node-server/app.js
+++ node-server/app.js
@@ -1,4 +1,5 @@
 const http = require('http');
+const fs = require('fs')
 
 // this creates a server object
 const server = http.createServer((req, res)=>{
@@ -7,12 +8,20 @@ const server = http.createServer((req, res)=>{
 
     // console.log(`url=>${req.url}`, req.method, req.headers)
     const url = req.url
+    const method = req.method
     if(url === '/'){
         res.setHeader('Content-Type', 'text/html')
         res.write('<html>')
         res.write('<head><title>Enter a message</title></head>')
         res.write('<body><form action="/message" method="POST"><input name="message"/><button type="submit">Submit</button></form></body>')
         res.write('</html>')
+        return res.end()
+    }
+
+    if(url === '/message' && method === 'POST'){
+        fs.writeFileSync('dummy-file.txt', 'Added dummy text')
+        res.statusCode = 302
+        res.setHeader('Location', '/')
         return res.end()
     }
 
