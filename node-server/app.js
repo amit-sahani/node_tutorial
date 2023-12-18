4bd744e3c0a766fafbec7da408a1a0c5c5ccd3e0
amit-sahani <amitg.one19@gmail.com>
2023-12-19 00:04:23 +0530

    routing request routing request and creating different response for different request route

--- node-server/app.js
+++ node-server/app.js
@@ -3,10 +3,20 @@ const http = require('http');
 // this creates a server object
 const server = http.createServer((req, res)=>{
     //this callback will run every time an http request comes
-    console.log('New request comes.')
+    // console.log('New request comes.')
 
-    console.log(`url=>${req.url}`, req.method, req.headers)
+    // console.log(`url=>${req.url}`, req.method, req.headers)
+    const url = req.url
+    if(url === '/'){
+        res.setHeader('Content-Type', 'text/html')
+        res.write('<html>')
+        res.write('<head><title>Enter a message</title></head>')
+        res.write('<body><form action="/message" method="POST"><input name="message"/><button type="submit">Submit</button></form></body>')
+        res.write('</html>')
+        return res.end()
+    }
 
+    // execute only if url is not '/'
     res.setHeader('Content-Type', 'text/html')
     res.write('<html>')
     res.write('<body>')
